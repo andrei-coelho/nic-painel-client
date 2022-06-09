1e525b324da5ca0096b2ea64a56f3cf33ab87dc1
unknown <andreifcoelho@gmail.com>
2022-06-09 12:54:13 -0300

    estatisticas do perfil dos entrevistados

---
+++ src/pages/pesquisas/ChartRosquinha.vue
@@ -0,0 +1,89 @@
+<template>
+    <div v-if="loaded">
+        <Doughnut
+            :key="att"
+            :chart-options="options"
+            :chart-data="chartData"
+            :chart-id="chartId"
+            :dataset-id-key="datasetIdKey"
+            :width="width"
+            :height="height"
+        />
+    </div>
+</template>
+
+<script>
+import { Doughnut } from 'vue-chartjs'
+import { Chart as ChartJS, ArcElement, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
+ChartJS.register(ArcElement, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+export default {
+    name: 'ChartRosquinha',
+    components: { Doughnut },
+    props: {
+        datasetsSend:Array,
+        labelsSend:Array,
+        chartId: {
+            type: String,
+            default: 'bar-chart'
+        },
+        datasetIdKey: {
+            type: String,
+            default: 'label'
+        },
+        width: {
+            type: Number,
+            default: 300
+        },
+        height: {
+            type: Number,
+            default: 300
+        },
+        cssClasses: {
+            default: '',
+            type: String
+        },
+        styles: {
+            type: Object,
+            default: () => {}
+        },
+        plugins: {
+            type: Object,
+            default: () => {}
+        }
+    },
+    
+
+    data() {
+        return {
+            att:0,
+            loaded:false,
+            chartData:{},
+            options:{
+                responsive: true,
+                maintainAspectRatio: false
+            }
+        }
+    },
+
+    mounted() {
+        
+        this.chartData =  {
+                labels: this.labelsSend,
+                datasets: this.datasetsSend
+            }
+
+        this.att++
+        this.loaded = true
+        console.log("aqui");
+    },
+
+    computed: {
+       
+        getOptions(){
+            return 
+        }
+    },
+}
+</script>
--- src/pages/pesquisas/StatComponent.vue
+++ src/pages/pesquisas/StatComponent.vue
@@ -2,7 +2,11 @@
     <v-container style="background-color:white;overflow-y: scroll; height: 100%;">
         <v-row>
             <v-col cols="8">
-                 <h3>Estatísticas</h3>
+                <h4>Estatísticas</h4>
+                <h2>{{ titulo }}</h2>
+                <p>Total de Respostas: {{ total }}</p>
+                <p>Criado em: {{ criado_em }}</p>
+                <p><b>{{ ativo == 1 ? 'ativa' : 'finalizada' }}</b></p>
             </v-col>
             <v-col cols="4">
                 <v-btn
@@ -25,8 +29,28 @@
                         <v-expansion-panel-text>
                             <v-container fluid>
                                 <v-row>
+                                    <v-col cols="12" md="4">
+                                        <v-row>
+                                            <v-col cols="6">
+                                                <v-text-field
+                                                    v-model="models.data['min']"
+                                                    label="Data (inicio)"
+                                                    placeholder="dd/mm/yyyy"
+                                                    variant="outlined"
+                                                ></v-text-field>
+                                            </v-col>
+                                            <v-col cols="6">
+                                                <v-text-field
+                                                    v-model="models.data['max']"
+                                                    label="Data (fim)"
+                                                    placeholder="dd/mm/yyyy"
+                                                    variant="outlined"
+                                                ></v-text-field>
+                                            </v-col>
+                                        </v-row>
+                                    </v-col>
                                     <v-col cols="12" md="4" v-for="filter,k in filters" :key="k">
-                                        <div v-if="filter.type == 'string'">
+                                        <div v-if="filter.field != 'data' && filter.type == 'string'">
                                             <v-select
                                                 v-model="models[filter.field]"
                                                 :items="filter.options"
@@ -42,7 +66,7 @@
                                                 variant="outlined"
                                             ></v-select>
                                         </div>
-                                        <div v-else>
+                                        <div v-else-if="filter.field != 'data'">
                                             <v-row>
                                                 <v-col cols="6">
                                                     <v-text-field
@@ -82,7 +106,37 @@
         </v-row>
 
 
+        
+        <div v-if="loadProfile">
+            <v-row>
+                <v-col cols="12">
+                    <div class="mx-auto d-block">
+                        <Loading />
+                    </div>
+                </v-col>
+            </v-row>
+        </div>
+        <div v-else>
+            <v-row>
+                <v-col cols="12"><h2 class="text-center">Perfil dos Entrevistados</h2></v-col>
+                <v-col cols="6" md="4" lg="3" v-for="profile,k in profileStats" :key="k">
+                    <h4 class="text-center">{{ profile.titulo }}</h4>
+                    <ChartRosquinha 
+                        :datasetsSend="[
+                            { 
+                                backgroundColor: profile.backgrounds,
+                                data: profile.datas
+                            }
+                        ]"
+                        :labelsSend="profile.labels"
+                    />
+                </v-col>
+            </v-row>
+        </div>
+        
+
         <v-row class="justify-center">
+            <v-col cols="12"><h2 class="text-center">Perguntas</h2></v-col>
             <v-col cols="12" md="10" v-for="pergunta,kp in perguntas" :key="kp">
                 <v-card
                     class="mx-auto"
@@ -93,13 +147,13 @@
                             {{ pergunta.valor }}
                         </p>
                         <v-progress-linear v-for="option,ko in pergunta.options" :key="ko"
-                            :model-value="Math.ceil((option.votos * 100) / total)"
+                            :model-value="option.votos > 0 ? Math.ceil((option.votos * 100) / total) : 0"
                             class="my-2"
                             color="light-blue"
                             height="25"
                             striped
                         >
-                        <p class="text-right"><strong class="text-right float-right">{{ option.valor }} - {{ Math.ceil((option.votos * 100) / total) }}%</strong></p>
+                        <p class="text-right"><strong class="text-right float-right">{{ option.valor }} - {{ option.votos > 0 ? Math.ceil((option.votos * 100) / total) : 0 }}%</strong></p>
                         </v-progress-linear>
                     </v-card-text>
                 </v-card>
@@ -109,52 +163,94 @@
 </template>
 
 <script>
+
+import ChartRosquinha from './ChartRosquinha.vue'
+import Loading from '../../components/LoadComponent.vue'
+
 export default {
+    
+    components:{ChartRosquinha, Loading},
+    
     props:{
         dataSend:Object
     },
+    
     created() {
-        this.listFilters();
+        this.listFilters()
         this.gerarEstatistica()
     },
+
     data() {
         return {
+            ativo: this.dataSend.ativo,
+            loadProfile: true,
+            colors:[
+                '#F44336', '#E91E63', '#9C27B0', '#673AB7', '#3F51B5', '#2196F3',
+                '#03A9F4', '#00BCD4', '#009688', '#4CAF50', '#8BC34A', '#CDDC39',
+                '#FFEB3B', '#FFC107', '#FF9800', '#FF5722', '#795548', '#607D8B',
+            ],
+            profileStats:[],
             perguntas:[],
+            filterArr:[],
             filters:[],
-            models:{},
+            models:{
+                data:{
+                    min:'',
+                    max:''
+                }
+            },
             pesquisa_id: this.dataSend.id,
             filtered: false,
+            titulo:"",
+            criado_em:"",
             total:0
         }
     },
+
     methods: {
+
         close(){
             this.$emit('closeComponent')
         },
+        
         async listFilters(){
             // get_profile_fields
             let res = await this.$request('client@pesquisas/get_profile_fields', {
                 pesquisa_id:this.pesquisa_id
             })
             if(res.error) return;
-            this.filters = res.data; 
+            this.filters = res.data;
+            this.filters.push({
+                field:'data',
+                type:'string'
+            })
             this.clean()
+            this.gerarEstatisticaProfile()
         },
+        
         fitrar(){
             this.filtered = true
-            this.gerarEstatistica();
+            this.criarArrayFiltro()
+            this.gerarEstatistica()
+            this.gerarEstatisticaProfile()
         },
+
         async gerarEstatistica(){
             //console.log(this.models);
             this.perguntas = [];
-            let filters = this.criarArrayFiltro();
+
             let res = await this.$request('client@pesquisas/estatistica_votos', {
                 pesquisa_id: this.pesquisa_id,
-                filters: filters
+                filters: this.filterArr
             })
+
             if(!res.error){
-                this.total = res.data.total_pessoas
+                
+                this.total     = res.data.total_pessoas
+                this.titulo    = res.data.titulo
+                this.criado_em = res.data.criado_em
                 let keys = Object.keys(res.data.perguntas);
+
                 for (let i = 0; i < keys.length; i++) {
                     let optionskey = Object.keys(res.data.perguntas[keys[i]].options)
                     let perguntaTemp = {
@@ -167,12 +263,55 @@ export default {
                     }
                     this.perguntas.push(perguntaTemp)
                 }
-                console.log(this.perguntas);
+                
             }
+
+        },
+
+        async gerarEstatisticaProfile(){
+            
+            this.loadProfile = true
+            this.profileStats = []
+
+            let res = await this.$request('client@pesquisas/estatistica_perfil', {
+                pesquisa_id: this.pesquisa_id,
+                filters: this.filterArr,
+                fields: this.filters.filter(item => item.field != 'data').map(item => item.field)
+            })
+
+            if(res.error) {
+                this.loadProfile = false
+                return;
+            }
+
+            let titulos = Object.keys(res.data);
+            
+            for (let i = 0; i < titulos.length; i++) {
+                
+                const titulo = titulos[i];
+                const objArr = res.data[titulo];
+                
+                let backgrounds = [];
+                for (let x = 0; x < objArr.length; x++) {
+                    backgrounds.push(this.colors[x])                    
+                }
+
+                this.profileStats.push({
+                    titulo:titulo,
+                    backgrounds:backgrounds,
+                    labels:objArr.map(item => item.label),
+                    datas: objArr.map(item => item.total)
+                })
+            }
+            
+            this.loadProfile = false
         },
 
         criarArrayFiltro(){
+            
             let res = [];
+            this.filterArr = [];
+
             for (let i = 0; i < this.filters.length; i++) {
                 
                 const fil = this.filters[i];
@@ -191,8 +330,20 @@ export default {
                     continue;
                 }
                 
+                if(fil.field == 'data' && 
+                (this.models.data.min != '' || this.models.data.max != '')){
+                    let obj = {
+                        field: fil.field,
+                        range: {
+                            min: this.models.data.min,
+                            max: this.models.data.max
+                        }
+                    }
+                    res.push(obj)
+                    continue;
+                }        
 
-                if(this.models[fil.field] != ''){
+                if(fil.field != 'data' && this.models[fil.field] != ''){
                     let obj = {
                         field: fil.field,
                         equals: this.models[fil.field]
@@ -202,11 +353,15 @@ export default {
 
             }
 
-            return res
+            this.filterArr = res
         },
 
         clean(reload = false){
             this.filtered = false
+            this.models.data = {
+                min:'',
+                max:'',
+            }
             this.filters.forEach(fil => {
                 if(fil.type == 'int'){
                     this.models[fil.field] = {}
@@ -214,7 +369,8 @@ export default {
                     this.models[fil.field]['max'] = ''
                     return;
                 }
-                this.models[fil.field] = ''
+                if(fil.field != 'data')
+                    this.models[fil.field] = ''
             })
             if(reload) this.gerarEstatistica()
         }
